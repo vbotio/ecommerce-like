beeaeb05cead6c09a81059b3438f9f2565b6a36a
vbotio <vitorboccio@gmail.com>
2018-08-16 11:37:20 +1000

    adding function to give random profile pic if user hasnt one

--- server/ecommercelike/models/user.js
+++ server/ecommercelike/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt-nodejs");
-
+const crypto = require('crypto');
 
 const UserSchema = new Schema({
     email: { type: String, unique: true, lowercase: true},
@@ -34,4 +34,14 @@ Schema.pre('save', function(next) {
 
 UserSchema.methods.comparePassword = function(password) {
     return bcrypt.compareSync(password, this.password);
+}
+
+UserSchema.methods.gravatar = function(size) {
+    if(!this.size) size = 200;
+    if(!this.email) {
+        return 'http://gravatar.com/avatar/?s' + size + '&d=retro'
+    } else {
+        var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+        return 'http://gravatar.com/avatar/' + md5 + '?s' + size + '&d=retro';
+    }
 }
