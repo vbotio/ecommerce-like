2b86a4d586cddd74354b7aa030e9997d90d6945b
vbotio <vitorboccio@gmail.com>
2018-08-16 11:23:31 +1000

    including function to encrypt and compare password

--- server/ecommercelike/models/user.js
+++ server/ecommercelike/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
+
 
 const UserSchema = new Schema({
     email: { type: String, unique: true, lowercase: true},
@@ -17,3 +19,19 @@ const UserSchema = new Schema({
     },
     created: { type: Date, default: Date.now }
 })
+
+Schema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.hash(user.password, null, null, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+    })
+})
+
+UserSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+}
