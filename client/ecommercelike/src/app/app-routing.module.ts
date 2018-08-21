0737004952c9d6e54246e09703ac16fc86e4ed57
vbotio <vitorboccio@gmail.com>
2018-08-21 11:34:02 +1000

    adding layout for login page

--- client/ecommercelike/src/app/app-routing.module.ts
+++ client/ecommercelike/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegistrationComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: "**",
